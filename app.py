30e1c3497fb12c38b0d8161f8c6c223268108e89
Devendar Singh Rawat <devendasinghrawat925@gmail.com>
2025-04-05 01:33:20 +0530

    punk_tab

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from nltk.stem.porter import PorterStemmer
 nltk_data_path = os.path.join(os.path.expanduser("~"), "nltk_data")
 nltk.data.path.append(nltk_data_path)
 
-nltk.download('punkt', download_dir=nltk_data_path)
+nltk.download('punkt_tab', download_dir=nltk_data_path)
 nltk.download('stopwords', download_dir=nltk_data_path)
 
 # Initialize stemmer
